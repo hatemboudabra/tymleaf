09b2d53c7bc1e74136c6d57911eb5aa798fc9375
hatemboudabra <132352084+hatemboudabra@users.noreply.github.com>
2023-11-05 22:55:39 +0100

    new commit

---
+++ src/main/java/com/example/tymleaf/TymleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.tymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TymleafApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/tymleaf/controller/HelloW.java
@@ -0,0 +1,14 @@
+package com.example.tymleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloW {
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello Hatem!");
+        return "helloworld";
+    }
+}
